6a81c2a569281e62a5ebff0c1a8ba290f25cba95
Dimitris Petrou <47597523+petroud@users.noreply.github.com>
2020-12-22 20:04:48 +0200

    Add files via upload

--- core/Pinger.java
+++ core/Pinger.java
@@ -74,6 +74,6 @@ public class Pinger {
         
         return bytes;
     }
-	
+
 
 }
--- gui/Frame.java
+++ gui/Frame.java
@@ -18,7 +18,6 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.util.concurrent.TimeUnit;
 import java.awt.event.ActionEvent;
 
 
@@ -32,16 +31,14 @@ public class Frame {
 	private JLabel iplbl;
 	
 	private Pinger pinger;
-
-	
-	private JLabel ipTargetLbl;
 	private JLabel macTargetLbl;
-	private JTextField ipTextField;
 	private JTextField MACtextField;
 	private JLabel warnLabel;
+	private JTextField broadcastField;
 	
 	public Frame() {
-		mainFrame = new JFrame("WakeOnLan Waker 1.0.0 | by dpetrou");
+		mainFrame = new JFrame();
+		mainFrame.setTitle("WakeOnLan Waker 1.0.2 | by dpetrou");
 		mainFrame.setSize(600,400);
 		mainFrame.setVisible(true);
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -60,55 +57,48 @@ public class Frame {
 		mainFrame.getContentPane().add(mainPanel);
 		
 		pageTitle = new JLabel("WakeOnLan Waker");
-		pageTitle.setFont(new Font("Microsoft JhengHei UI", Font.BOLD, 19));
+		pageTitle.setFont(new Font("Tahoma", Font.BOLD, 20));
 		pageTitle.setHorizontalAlignment(SwingConstants.CENTER);
-		pageTitle.setBounds(201, 11, 184, 35);
+		pageTitle.setBounds(160, 11, 254, 35);
 		mainPanel.add(pageTitle);
 		
 		yourIPlbl = new JLabel("Your local IP:");
-		yourIPlbl.setFont(new Font("Microsoft JhengHei UI", Font.PLAIN, 13));
-		yourIPlbl.setBounds(201, 57, 101, 14);
+		yourIPlbl.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		yourIPlbl.setBounds(194, 58, 120, 14);
 		mainPanel.add(yourIPlbl);
 		
 		iplbl = new JLabel(pinger.getLocalIP());
 		iplbl.setHorizontalAlignment(SwingConstants.CENTER);
-		iplbl.setFont(new Font("Microsoft JhengHei UI", Font.BOLD, 13));
+		iplbl.setFont(new Font("Tahoma", Font.BOLD, 14));
 		iplbl.setForeground(new Color(25, 25, 112));
-		iplbl.setBounds(266, 57, 153, 14);
+		iplbl.setBounds(261, 58, 153, 14);
 		mainPanel.add(iplbl);
-		
-		ipTargetLbl = new JLabel("Target IP: ");
-		ipTargetLbl.setForeground(new Color(0, 0, 0));
-		ipTargetLbl.setHorizontalAlignment(SwingConstants.RIGHT);
-		ipTargetLbl.setFont(new Font("Microsoft JhengHei UI", Font.PLAIN, 12));
-		ipTargetLbl.setBounds(131, 133, 76, 22);
-		mainPanel.add(ipTargetLbl);
 		
 		macTargetLbl = new JLabel("Target MAC: ");
 		macTargetLbl.setHorizontalAlignment(SwingConstants.RIGHT);
-		macTargetLbl.setFont(new Font("Microsoft JhengHei UI", Font.PLAIN, 12));
-		macTargetLbl.setBounds(131, 186, 76, 22);
+		macTargetLbl.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		macTargetLbl.setBounds(126, 198, 110, 22);
 		mainPanel.add(macTargetLbl);
 		
-		ipTextField = new JTextField();
-		ipTextField.setBorder(new CompoundBorder(new LineBorder(new Color(105, 105, 105)), new LineBorder(new Color(255, 255, 255), 5)));
-		ipTextField.setFont(new Font("Microsoft JhengHei UI", Font.BOLD, 12));
-		ipTextField.setBounds(217, 127, 184, 35);
-		mainPanel.add(ipTextField);
-		ipTextField.setColumns(10);
+		broadcastField = new JTextField("192.168.1.255");
+		broadcastField.setFont(new Font("Tahoma", Font.BOLD, 13));
+		broadcastField.setColumns(10);
+		broadcastField.setBorder(new CompoundBorder(new LineBorder(new Color(105, 105, 105)), new LineBorder(new Color(255, 255, 255), 5)));
+		broadcastField.setBounds(261, 147, 197, 35);
+		mainPanel.add(broadcastField);
 		
 		MACtextField = new JTextField("");
-		MACtextField.setFont(new Font("Microsoft JhengHei UI", Font.BOLD, 12));
+		MACtextField.setFont(new Font("Tahoma", Font.BOLD, 13));
 		MACtextField.setBorder(new CompoundBorder(new LineBorder(new Color(105, 105, 105)), new LineBorder(new Color(255, 255, 255), 5)));
 		MACtextField.setColumns(10);
-		MACtextField.setBounds(217, 180, 184, 35);
+		MACtextField.setBounds(261, 193, 197, 35);
 		mainPanel.add(MACtextField);
 		
-		warnLabel = new JLabel("");
-		warnLabel.setFont(new Font("Microsoft JhengHei UI", Font.BOLD, 13));
+		warnLabel = new JLabel();
+		warnLabel.setFont(new Font("Tahoma", Font.BOLD, 13));
 		warnLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		warnLabel.setForeground(new Color(128, 0, 0));
-		warnLabel.setBounds(246, 280, 123, 25);
+		warnLabel.setBounds(194, 310, 187, 25);
 		mainPanel.add(warnLabel);
 		
 		JButton wakeupBTN = new JButton("Wake Up");
@@ -119,7 +109,7 @@ public class Frame {
 			public void actionPerformed(ActionEvent e) {
 				try {
 					warnLabel.setText("");
-					pinger.wakeUp(ipTextField.getText(), MACtextField.getText());
+				    pinger.wakeUp(broadcastField.getText().trim(), MACtextField.getText().trim());
 					warnLabel.setForeground(new Color(46,139,87));
 					warnLabel.setText("MagicPacket sent");					
 				} catch (Exception e1) {
@@ -142,9 +132,15 @@ public class Frame {
 		});
 		wakeupBTN.setFocusPainted(false);
 		wakeupBTN.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		wakeupBTN.setFont(new Font("Microsoft JhengHei UI", Font.PLAIN, 13));
-		wakeupBTN.setBounds(252, 238, 110, 35);
+		wakeupBTN.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		wakeupBTN.setBounds(348, 266, 110, 35);
 		mainPanel.add(wakeupBTN);
+		
+		JLabel lblBroadcastNetwork = new JLabel("Broadcast Network:");
+		lblBroadcastNetwork.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblBroadcastNetwork.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblBroadcastNetwork.setBounds(77, 152, 153, 22);
+		mainPanel.add(lblBroadcastNetwork);
 		
 	}
 	
